7c1e3739a4503fc0ebc54b29770e98223ec43769
Amrutha Deeti <amruthadeeti@gmail.com>
2026-03-25 22:06:39 -0500

    Add Spring Boot backend project

---
+++ qualityops-backend/src/main/java/qualityops_backend/QualityopsBackendApplication.java
@@ -0,0 +1,13 @@
+package qualityops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityopsBackendApplication.class, args);
+	}
+
+}
